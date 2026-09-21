92fabddbd12142dc473a90131c3ced9940db5748
Ritam Naskar <ritamnaskar.bca.2022@gmail.com>
2026-09-21 12:22:43 +0530

    Update code\

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
-import webbrowser
+# import webbrowser
 from urllib.parse import quote_plus
+import subprocess
+import pywhatkit
 
 engine = pyttsx3.init()
 engine.setProperty("rate", 160)
@@ -12,7 +14,7 @@ recognizer = sr.Recognizer()
 
 # Function to speak
 def speak(text):
-    print("Pyvoice:",text)
+    print("Emo:",text)
     engine.say(text)
     engine.runAndWait()
     
@@ -57,14 +59,14 @@ while True:
 
     if not query:
         continue
-    if "hello" in query or "hi" in query:
+    if "hello" in query or "hi" in query or "hey" in query:
         speak("Hello! Nice to talk to you")
 
     elif query == "how are you":
         speak("I am doing great. Thank you for asking!")
 
     elif "your name" in query:
-        speak("My name is Pyvoice")
+        speak("My name is Emo")
 
     elif "time" in query:
         current_time = datetime.datetime.now().strftime("%I:%M %p")
@@ -76,18 +78,33 @@ while True:
         current_day = datetime.datetime.now().strftime("%A")
         speak(f"Today is {current_day}")
 
-    elif "youtube" in query:
-        speak("Opening Youtube")
-        webbrowser.open("https://www.youtube.com")
-
     elif "search for" in query:
-        search_query = query.replace("searc for", "",1).strip()
+        search_query = query.replace("search for", "",1).strip()
         if search_query:
-            speak(f"Searching Google {search_query}")
-            webbrowser.open("https://www.google.com/search?q=" + quote_plus(search_query))
-
+            speak(f"Searching Google for {search_query}")
+            subprocess.Popen(["cmd","/c","start","chrome","https://www.google.com/search?q=" + quote_plus(search_query)])
         else:
             speak("What should I search for?")
+
+    elif "open youtube" in query:
+        speak("Opening YouTube")    
+        subprocess.Popen(["cmd", "/c", "start", "chrome","https://www.youtube.com"])
+
+    elif "play" in query:
+        song = query.replace("play","",1).strip()
+
+        if song.endswith(" song"):
+            song = song[:-5].strip()
+            
+
+        if song:
+            speak(f"Searching Youtube for {song}")   
+            subprocess.Popen(["cmd", "/c", "start","chrome","https://www.youtube.com/results?search_query=" + quote_plus(song)])
+        else:
+            speak("What should I play for?")
+
+    el
+    
     
     elif "stop" in query or "exit" in query or "goodbye" in query or "bye" in query:
         speak("Goodbye! See you again.")
